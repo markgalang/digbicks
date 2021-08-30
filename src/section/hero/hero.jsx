b3bc893ef23f3c532b8f525bba0b2b8fdaf3e7bd
Mark Galang <galangmarkg@gmail.com>
2021-08-30 21:24:49 +0800

    Add Logo

--- src/section/hero/hero.jsx
+++ src/section/hero/hero.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./hero.css";
 import FatherOfAllBicks from "assets/images/fatherOfBicks.png";
+import DigBicks from "assets/images/logo.png";
 import { PlayCircle } from "react-feather";
 import { X } from "react-feather";
 
@@ -8,6 +9,11 @@ function Hero({ isOpen, setIsOpen }) {
   return (
     <div className="heroContainer">
       {isOpen && <X className="closeBtn" onClick={() => setIsOpen(false)} />}
+
+      <div className="logoContainer">
+        <img className="logo" src={DigBicks} alt="DigBicks" />
+      </div>
+
       <img
         className="fatherDickImg"
         alt="Father of all Bicks"
