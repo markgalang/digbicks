0216f5cc1145584a7728a7fc9c16192db1de2e38
Mark Galang <galangmarkg@gmail.com>
2021-09-02 21:47:36 +0800

    Add Video

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import VideoPlayback from "components/VideoPlayback/VideoPlayback";
 import Footer from "components/Footer/Footer";
 import Hero from "./section/hero/hero";
 import Collection from "./section/Collection/Collection";
+import HeroVideo from "section/heroVideo/heroVideo";
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div>
+      <HeroVideo />
       <Hero isOpen={isOpen} setIsOpen={setIsOpen} />
       <VideoPlayback isOpen={isOpen} setIsOpen={setIsOpen} />
       <Collection />
