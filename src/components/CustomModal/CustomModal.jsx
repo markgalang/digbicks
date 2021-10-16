2a41482c0ae82169d24c9bd2c6773a955d328668
Mark Galang <galangmarkg@gmail.com>
2021-10-17 03:43:12 +0800

    Add Styling on Custom Modal

--- src/components/CustomModal/CustomModal.jsx
+++ src/components/CustomModal/CustomModal.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import Modal from "react-bootstrap/Modal";
 import { useSelector, useDispatch } from "react-redux";
 import { hideModal } from "redux/actions";
-import { MESSAGE_TYPE } from "util/enums";
+import { MESSAGE_TYPE, OPENSEA_ACCOUNT_PAGE } from "util/enums";
 import "./CustomModal.css";
+import { X } from "react-feather";
 
 function CustomModal() {
   const { isModalOpen, type, message } = useSelector((state) => state.modal);
   const dispatch = useDispatch();
-  const isCentered = type === MESSAGE_TYPE.SUCCESS;
 
   const _handleClose = () => {
     dispatch(hideModal());
@@ -20,14 +20,32 @@ function CustomModal() {
       onHide={_handleClose}
       backdrop="static"
       keyboard={false}
-      centered={isCentered}
+      centered
       className="customModal"
     >
       {" "}
-      <div variant="secondary" onClick={_handleClose}>
-        Close
-      </div>
-      <Modal.Body>{message}</Modal.Body>
+      <Modal.Body className="customModalBody">
+        <X className="modalCloseBtn" onClick={_handleClose} />
+
+        <h2 className="modalTitle">You got the D!</h2>
+        <br />
+        <p className="modalDescription">
+          Your minted DigBicks NFT will be available on your{" "}
+          <a
+            className="modalLink"
+            target="_blank"
+            rel="noopener noreferrer"
+            href={OPENSEA_ACCOUNT_PAGE}
+          >
+            OpenSea account
+          </a>{" "}
+          in a few minutes
+        </p>
+
+        <button className="modalButton" onClick={_handleClose}>
+          Close
+        </button>
+      </Modal.Body>
     </Modal>
   );
 }
