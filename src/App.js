cdea7fbdfd50208d1c3c305e2ab714040a06df40
Mark Galang <galangmarkg@gmail.com>
2021-10-17 19:46:34 +0800

    Add Google Analytics

--- src/App.js
+++ src/App.js
@@ -4,6 +4,28 @@ import TermsAndCondition from "pages/termsAndCondition/TermsAndCondition.jsx";
 import ScrollToTop from "components/ScrollTotop/ScrollToTop";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBHD0R3XwxC2A7cmogyNeOPVfXO5t6N2iQ",
+  authDomain: "digb-prod-1.firebaseapp.com",
+  projectId: "digb-prod-1",
+  storageBucket: "digb-prod-1.appspot.com",
+  messagingSenderId: "215038037266",
+  appId: "1:215038037266:web:5185faf370faaec8cf77cb",
+  measurementId: "G-3FNM0497LQ",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 function App() {
   return (
     <Router>
